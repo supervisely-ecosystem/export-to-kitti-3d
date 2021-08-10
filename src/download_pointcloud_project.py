10874ba696ded723560fb409f8ac853d9de76d00
Pavel Bartsits <48913536+cxnt@users.noreply.github.com>
2021-08-10 16:11:16 +0300

    rm unused variable

--- src/download_pointcloud_project.py
+++ src/download_pointcloud_project.py
@@ -6,8 +6,7 @@ from supervisely_lib.project.pointcloud_project import download_pointcloud_proje
 
 def start(project_id, dest_dir):
     shutil.rmtree(dest_dir, ignore_errors=True)  # WARNING!
-    api = sly.Api.from_env()
-    download_pointcloud_project(api, project_id, dest_dir, dataset_ids=None, download_items=True, log_progress=True)
+    download_pointcloud_project(g.api, project_id, dest_dir, dataset_ids=None, download_items=True, log_progress=True)
 
     sly.logger.info('PROJECT_DOWNLOADED', extra={'dest_dir': dest_dir,
                                                  'datasets': [x for x in os.listdir(dest_dir) if 'json' not in x]})
