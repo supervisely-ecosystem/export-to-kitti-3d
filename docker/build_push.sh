dc6a35644d2a7347d2023712435c49b05cd651ae
Max Kolomeychenko <maxim.kolomeychenko@gmail.com>
2022-09-21 12:36:19 +0300

    Update build_push.sh

--- docker/build_push.sh
+++ docker/build_push.sh
@@ -1,2 +1,2 @@
-docker build -t supervisely/import-export-kitty:1.0.1 . && \
-docker push supervisely/import-export-kitty:1.0.1
+docker build -t supervisely/import-export-kitty:1.0.2 . && \
+docker push supervisely/import-export-kitty:1.0.2
