1b2f04e2e32578a5391a0cdea096a761f83766fb
Max Kolomeychenko <maxim.kolomeychenko@gmail.com>
2022-12-29 11:01:16 +0000

    batch_size=1

--- src/download_pointcloud_project.py
+++ src/download_pointcloud_project.py
@@ -7,7 +7,7 @@ from supervisely.project.pointcloud_project import download_pointcloud_project
 
 def start(project_id, dest_dir):
     shutil.rmtree(dest_dir, ignore_errors=True)  # WARNING!
-    download_pointcloud_project(g.api, project_id, dest_dir, dataset_ids=None, download_items=True, log_progress=True)
+    download_pointcloud_project(g.api, project_id, dest_dir, dataset_ids=None, download_items=True, batch_size=1, log_progress=True)
 
     sly.logger.info('PROJECT_DOWNLOADED', extra={'dest_dir': dest_dir,
                                                  'datasets': [x for x in os.listdir(dest_dir) if 'json' not in x]})
