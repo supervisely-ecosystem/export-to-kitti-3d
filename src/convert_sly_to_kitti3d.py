b07d59fd1f7a6151cca9711fe5aca1959c1fd425
GoldenAnpu <seryios.sych@gmail.com>
2023-09-07 23:30:03 +0200

    add mapping for pcd, ann, images

--- src/convert_sly_to_kitti3d.py
+++ src/convert_sly_to_kitti3d.py
@@ -36,35 +36,95 @@ def sort_kitti():
     sly.fs.mkdir(temp_train_img_dir)
     sly.fs.mkdir(temp_test_img_dir)
 
-    datasets = [ds for ds in os.listdir(g.sly_base_dir) if os.path.isdir(os.path.join(g.sly_base_dir, ds))]
+    datasets = [
+        ds for ds in os.listdir(g.sly_base_dir) if os.path.isdir(os.path.join(g.sly_base_dir, ds))
+    ]
     for ds in datasets:
         ann_dir = os.path.join(g.sly_base_dir, ds, "ann")
-        ann_paths = [os.path.join(ann_dir, ann_path) for ann_path in os.listdir(ann_dir) if
-                     os.path.isfile(os.path.join(ann_dir, ann_path))]
+        ann_paths = [
+            os.path.join(ann_dir, ann_path)
+            for ann_path in os.listdir(ann_dir)
+            if os.path.isfile(os.path.join(ann_dir, ann_path))
+        ]
+        ann_paths = sorted(ann_paths)
 
         pcd_dir = os.path.join(g.sly_base_dir, ds, "pointcloud")
-        pcd_paths = [os.path.join(pcd_dir, pcd_path) for pcd_path in os.listdir(pcd_dir) if
-                     os.path.isfile(os.path.join(pcd_dir, pcd_path))]
+        pcd_paths = [
+            os.path.join(pcd_dir, pcd_path)
+            for pcd_path in os.listdir(pcd_dir)
+            if os.path.isfile(os.path.join(pcd_dir, pcd_path))
+        ]
+        pcd_paths = sorted(pcd_paths)
 
         img_dir = os.path.join(g.sly_base_dir, ds, "related_images")
         img_paths = None
         if os.path.isdir(img_dir):
-            img_paths = [os.path.join(img_dir, img_path) for img_path in os.listdir(img_dir) if
-                         os.path.isdir(os.path.join(img_dir, img_path))]
+            img_paths = [
+                os.path.join(img_dir, img_path)
+                for img_path in os.listdir(img_dir)
+                if os.path.isdir(os.path.join(img_dir, img_path))
+            ]
+            img_paths = sorted(img_paths)
 
-        for idx, (ann_path, pcd_path) in enumerate(zip(sorted(ann_paths), sorted(pcd_paths))):
-            ann_json = sly.json.load_json_file(ann_path)
-            ann = sly.PointcloudAnnotation.from_json(ann_json, meta)
-            if len(ann.figures) > 0:
-                shutil.copy(ann_path, os.path.join(temp_train_ann_dir, os.path.basename(os.path.normpath(ann_path))))
-                shutil.copy(pcd_path, os.path.join(temp_train_pcd_dir, os.path.basename(os.path.normpath(pcd_path))))
-                if img_paths is not None:
-                    shutil.copytree(img_paths[idx], os.path.join(temp_train_img_dir, os.path.basename(os.path.normpath(img_paths[idx]))))
-            if len(ann.figures) == 0:
-                shutil.copy(ann_path, os.path.join(temp_test_ann_dir, os.path.basename(os.path.normpath(ann_path))))
-                shutil.copy(pcd_path, os.path.join(temp_test_pcd_dir, os.path.basename(os.path.normpath(pcd_path))))
-                if img_paths is not None:
-                    shutil.copytree(img_paths[idx], os.path.join(temp_test_img_dir, os.path.basename(os.path.normpath(img_paths[idx]))))
+        file_mapping = {}
+        for ann_path, pcd_path in zip(ann_paths, pcd_paths):
+            common_filename = os.path.splitext(os.path.basename(pcd_path))[0]
+            file_mapping[common_filename] = {"ann": ann_path, "pcd": pcd_path, "img": None}
+
+        if img_paths:
+            for img_path in img_paths:
+                common_filename = os.path.basename(img_path).split("_")[0]
+                if common_filename in file_mapping:
+                    file_mapping[common_filename]["img"] = img_path
+
+        for common_filename, files in file_mapping.items():
+            ann_path = files["ann"]
+            pcd_path = files["pcd"]
+            img_path = files["img"]
+
+            if ann_path and pcd_path:
+                ann_json = sly.json.load_json_file(ann_path)
+                ann = sly.PointcloudAnnotation.from_json(ann_json, meta)
+                if len(ann.figures) > 0:
+                    shutil.copy(
+                        ann_path,
+                        os.path.join(
+                            temp_train_ann_dir, os.path.basename(os.path.normpath(ann_path))
+                        ),
+                    )
+                    shutil.copy(
+                        pcd_path,
+                        os.path.join(
+                            temp_train_pcd_dir, os.path.basename(os.path.normpath(pcd_path))
+                        ),
+                    )
+                    if img_path:
+                        shutil.copytree(
+                            img_path,
+                            os.path.join(
+                                temp_train_img_dir, os.path.basename(os.path.normpath(img_path))
+                            ),
+                        )
+                else:
+                    shutil.copy(
+                        ann_path,
+                        os.path.join(
+                            temp_test_ann_dir, os.path.basename(os.path.normpath(ann_path))
+                        ),
+                    )
+                    shutil.copy(
+                        pcd_path,
+                        os.path.join(
+                            temp_test_pcd_dir, os.path.basename(os.path.normpath(pcd_path))
+                        ),
+                    )
+                    if img_path:
+                        shutil.copytree(
+                            img_path,
+                            os.path.join(
+                                temp_test_img_dir, os.path.basename(os.path.normpath(img_path))
+                            ),
+                        )
 
     path_to_keyIdMap = os.path.join(g.sly_base_dir, "key_id_map.json")
     shutil.copy(path_to_meta, os.path.join(temp_proj_dir, "meta.json"))
@@ -87,8 +147,16 @@ def check_dataset_files(project_dir, project_name=None):
         if os.path.isdir(dataset_path):
             for subdir in os.listdir(dataset_path):
                 subdir = os.path.join(dataset_path, subdir)
-                dir_files_cnt = len([file for file in os.listdir(subdir) if os.path.isfile(os.path.join(subdir, file))])
-                subdir_dirs_cnt = len([dir for dir in os.listdir(subdir) if os.path.isdir(os.path.join(subdir, dir))])
+                dir_files_cnt = len(
+                    [
+                        file
+                        for file in os.listdir(subdir)
+                        if os.path.isfile(os.path.join(subdir, file))
+                    ]
+                )
+                subdir_dirs_cnt = len(
+                    [dir for dir in os.listdir(subdir) if os.path.isdir(os.path.join(subdir, dir))]
+                )
                 if dir_files_cnt == 0 and subdir_dirs_cnt == 0:
                     remove_dir(subdir)
             if len(os.listdir(dataset_path)) == 0:
@@ -97,16 +165,16 @@ def check_dataset_files(project_dir, project_name=None):
             continue
 
 
-def kitti_paths(path, ds_name, mode='write'):
+def kitti_paths(path, ds_name, mode="write"):
     path = os.path.join(path, ds_name)
-    bin_dir = os.path.join(path, 'velodyne')
-    image_dir = os.path.join(path, 'image_2')
+    bin_dir = os.path.join(path, "velodyne")
+    image_dir = os.path.join(path, "image_2")
     label_dir = None
     if ds_name == "training":
-        label_dir = os.path.join(path, 'label_2')
-    calib_dir = os.path.join(path, 'calib')
+        label_dir = os.path.join(path, "label_2")
+    calib_dir = os.path.join(path, "calib")
 
-    if mode == 'write' or mode == 'w':
+    if mode == "write" or mode == "w":
         assert not os.path.exists(path), "Dataset already exists. Remove directory before writing"
         os.mkdir(path)
         os.mkdir(bin_dir)
@@ -149,20 +217,22 @@ def annotation_to_kitti_label(annotation_path, calib_path, kiiti_label_path, met
         position = geometry.position
         rotation = geometry.rotation
 
-        obj = BEVBox3D(center=np.array([float(position.x), float(position.y), float(position.z)]),
-                       size=np.array([float(dimensions.x), float(dimensions.z), float(dimensions.y)]),
-                       yaw=np.array(float(-rotation.z)),
-                       label_class=class_name,
-                       confidence=1.0,
-                       world_cam=calib['world_cam'],
-                       cam_img=calib['cam_img'])
+        obj = BEVBox3D(
+            center=np.array([float(position.x), float(position.y), float(position.z)]),
+            size=np.array([float(dimensions.x), float(dimensions.z), float(dimensions.y)]),
+            yaw=np.array(float(-rotation.z)),
+            label_class=class_name,
+            confidence=1.0,
+            world_cam=calib["world_cam"],
+            cam_img=calib["cam_img"],
+        )
 
         objects.append(obj)
 
-    with open(kiiti_label_path, 'w') as f:
+    with open(kiiti_label_path, "w") as f:
         for box in objects:
             f.write(box.to_kitti_format(box.confidence))
-            f.write('\n')
+            f.write("\n")
 
 
 def mkline(arr):
@@ -170,14 +240,18 @@ def mkline(arr):
 
 
 def write_lines_to_txt(lines, path):
-    with open(path, 'w') as f:
+    with open(path, "w") as f:
         for line in lines:
-            f.write(line + '\n')
+            f.write(line + "\n")
 
 
 def gen_calib_from_img_meta(img_meta, path):
-    extrinsic_matrix = np.array(img_meta['meta']['sensorsData']['extrinsicMatrix'], dtype=np.float32)
-    intrinsic_matrix = np.array(img_meta['meta']['sensorsData']['intrinsicMatrix'], dtype=np.float32)
+    extrinsic_matrix = np.array(
+        img_meta["meta"]["sensorsData"]["extrinsicMatrix"], dtype=np.float32
+    )
+    intrinsic_matrix = np.array(
+        img_meta["meta"]["sensorsData"]["intrinsicMatrix"], dtype=np.float32
+    )
 
     cam_img = intrinsic_matrix.reshape(3, 3)
     cam_img = np.hstack((cam_img, np.zeros((3, 1)))).flatten()
@@ -190,7 +264,8 @@ def gen_calib_from_img_meta(img_meta, path):
         f"P3: {empty_line}",
         f"R0_rect: {mkline(np.eye(3, dtype=np.float32).flatten())}",
         f"Tr_velo_to_cam: {mkline(extrinsic_matrix)}",
-        f"Tr_imu_to_velo: {empty_line}"]
+        f"Tr_imu_to_velo: {empty_line}",
+    ]
 
     write_lines_to_txt(lines, path)
 
@@ -211,14 +286,16 @@ def convert(project_dir, kitti_dataset_path, exclude_items=[]):
                 continue
 
             item_path, related_images_dir, ann_path = dataset_fs.get_item_paths(item_name)
-            item_name_without_ext = item_name.split('.')[0]
+            item_name_without_ext = item_name.split(".")[0]
             if dataset_fs.name == "training":
-                label_path = os.path.join(label_dir, item_name_without_ext + '.txt')
-            calib_path = os.path.join(calib_dir, item_name_without_ext + '.txt')
-            bin_path = os.path.join(bin_dir, item_name_without_ext + '.bin')
-            image_path = os.path.join(image_dir, item_name_without_ext + '.png')
+                label_path = os.path.join(label_dir, item_name_without_ext + ".txt")
+            calib_path = os.path.join(calib_dir, item_name_without_ext + ".txt")
+            bin_path = os.path.join(bin_dir, item_name_without_ext + ".bin")
+            image_path = os.path.join(image_dir, item_name_without_ext + ".png")
             if not os.path.isdir(related_images_dir):
-                sly.logger.warn(f"{item_name} is missing photo context, can't generate calibration file, item will be skipped")
+                sly.logger.warn(
+                    f"{item_name} is missing photo context, can't generate calibration file, item will be skipped"
+                )
                 continue
 
             try:
@@ -230,12 +307,20 @@ def convert(project_dir, kitti_dataset_path, exclude_items=[]):
 
             pcd_to_bin(item_path, bin_path)
             if dataset_fs.name == "training":
-                annotation_to_kitti_label(ann_path, calib_path=calib_path, kiiti_label_path=label_path,
-                                          meta=project_fs.meta)
+                annotation_to_kitti_label(
+                    ann_path,
+                    calib_path=calib_path,
+                    kiiti_label_path=label_path,
+                    meta=project_fs.meta,
+                )
             shutil.copy(src=related_img_path, dst=image_path)
             sly.logger.info(f"{item_name} converted to kitti .bin")
             progress.iter_done_report()
     check_dataset_files(g.kitti_base_dir)
     if len(os.listdir(g.kitti_base_dir)) == 0:
-        raise Exception("Photo context is necessary to create a calibration file for the KITTI format, all pointclouds without photo context were disregarded. Nothing to convert")
-    sly.logger.info(f"Dataset has been converted to KITTI format and saved to Team Files: {kitti_dataset_path}")
+        raise Exception(
+            "Photo context is necessary to create a calibration file for the KITTI format, all pointclouds without photo context were disregarded. Nothing to convert"
+        )
+    sly.logger.info(
+        f"Dataset has been converted to KITTI format and saved to Team Files: {kitti_dataset_path}"
+    )
