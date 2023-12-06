5d299805b157f005df7e84572b1f50b4c9c4b3fb
almaz <almazgimaev@gmail.com>
2023-12-06 03:22:10 +0100

    refactor functions

--- src/convert_sly_to_kitti3d.py
+++ src/convert_sly_to_kitti3d.py
@@ -8,33 +8,53 @@ from supervisely.io.fs import remove_dir
 from open3d._ml3d.datasets.utils import BEVBox3D
 
 
+def create_temp_dirs(episodes=False):
+    temp_dir = os.path.join(g.storage_dir, "temp_dir")
+    proj_dir = os.path.join(temp_dir, g.project_name)
+    train_dir = os.path.join(proj_dir, "training")
+    test_dir = os.path.join(proj_dir, "testing")
+    if episodes:
+        train_ann = os.path.join(train_dir, "annotation.json")
+        test_ann = os.path.join(test_dir, "annotation.json")
+        train_ann_dir = None
+        test_ann_dir = None
+    else:
+        train_ann = None
+        test_ann = None
+        train_ann_dir = os.path.join(train_dir, "ann")
+        test_ann_dir = os.path.join(test_dir, "ann")
+    train_pcd_dir = os.path.join(train_dir, "pointcloud")
+    test_pcd_dir = os.path.join(test_dir, "pointcloud")
+    train_img_dir = os.path.join(train_dir, "related_images")
+    test_img_dir = os.path.join(test_dir, "related_images")
+
+    sly.fs.mkdir(temp_dir, remove_content_if_exists=True)
+    sly.fs.mkdir(proj_dir)
+    sly.fs.mkdir(train_dir)
+    sly.fs.mkdir(test_dir)
+
+    if not episodes:
+        sly.fs.mkdir(train_ann_dir)
+        sly.fs.mkdir(test_ann_dir)
+    sly.fs.mkdir(train_pcd_dir)
+    sly.fs.mkdir(test_pcd_dir)
+    sly.fs.mkdir(train_img_dir)
+    sly.fs.mkdir(test_img_dir)
+
+    train_paths = [train_dir, train_pcd_dir, train_img_dir, train_ann_dir, train_ann]
+    test_paths = [test_dir, test_pcd_dir, test_img_dir, test_ann_dir, test_ann]
+
+    return temp_dir, proj_dir, train_paths, test_paths
+
+
 def sort_pointclouds_for_kitti():
     path_to_meta = os.path.join(g.sly_base_dir, "meta.json")
     meta_json = sly.json.load_json_file(path_to_meta)
     meta = sly.ProjectMeta.from_json(meta_json)
 
-    temp_dir = os.path.join(g.storage_dir, "temp_dir")
-    temp_proj_dir = os.path.join(temp_dir, g.project_name)
-    temp_train_dir = os.path.join(temp_proj_dir, "training")
-    temp_test_dir = os.path.join(temp_proj_dir, "testing")
-    temp_train_ann_dir = os.path.join(temp_train_dir, "ann")
-    temp_test_ann_dir = os.path.join(temp_test_dir, "ann")
-    temp_train_pcd_dir = os.path.join(temp_train_dir, "pointcloud")
-    temp_test_pcd_dir = os.path.join(temp_test_dir, "pointcloud")
-    temp_train_img_dir = os.path.join(temp_train_dir, "related_images")
-    temp_test_img_dir = os.path.join(temp_test_dir, "related_images")
-
-    sly.fs.mkdir(temp_dir, remove_content_if_exists=True)
-    sly.fs.mkdir(temp_proj_dir)
-    sly.fs.mkdir(temp_train_dir)
-    sly.fs.mkdir(temp_test_dir)
-
-    sly.fs.mkdir(temp_train_ann_dir)
-    sly.fs.mkdir(temp_test_ann_dir)
-    sly.fs.mkdir(temp_train_pcd_dir)
-    sly.fs.mkdir(temp_test_pcd_dir)
-    sly.fs.mkdir(temp_train_img_dir)
-    sly.fs.mkdir(temp_test_img_dir)
+    temp_dir, proj_dir, train_paths, test_paths = create_temp_dirs()
+    _, train_pcd_dir, train_img_dir, train_ann_dir, _ = train_paths
+    _, test_pcd_dir, test_img_dir, test_ann_dir, _ = test_paths
 
     datasets = [
         ds for ds in os.listdir(g.sly_base_dir) if os.path.isdir(os.path.join(g.sly_base_dir, ds))
@@ -86,27 +106,21 @@ def sort_pointclouds_for_kitti():
             if ann_path and pcd_path:
                 ann_json = sly.json.load_json_file(ann_path)
                 ann = sly.PointcloudAnnotation.from_json(ann_json, meta)
-                temp_ann_dir = temp_train_ann_dir if len(ann.figures) > 0 else temp_test_ann_dir
-                temp_pcd_dir = temp_train_pcd_dir if len(ann.figures) > 0 else temp_test_pcd_dir
-                temp_img_dir = temp_train_img_dir if len(ann.figures) > 0 else temp_test_img_dir
+                temp_ann_dir = train_ann_dir if len(ann.figures) > 0 else test_ann_dir
+                temp_pcd_dir = train_pcd_dir if len(ann.figures) > 0 else test_pcd_dir
+                temp_img_dir = train_img_dir if len(ann.figures) > 0 else test_img_dir
 
-                shutil.copy(
-                    ann_path,
-                    os.path.join(temp_ann_dir, os.path.basename(os.path.normpath(ann_path))),
-                )
-                shutil.copy(
-                    pcd_path,
-                    os.path.join(temp_pcd_dir, os.path.basename(os.path.normpath(pcd_path))),
-                )
+                ann_file_name = os.path.basename(os.path.normpath(ann_path))
+                shutil.copy(ann_path, os.path.join(temp_ann_dir, ann_file_name))
+                pcd_file_name = os.path.basename(os.path.normpath(pcd_path))
+                shutil.copy(pcd_path, os.path.join(temp_pcd_dir, pcd_file_name))
                 if img_path:
-                    shutil.copytree(
-                        img_path,
-                        os.path.join(temp_img_dir, os.path.basename(os.path.normpath(img_path))),
-                    )
+                    file_name = os.path.basename(os.path.normpath(img_path))
+                    shutil.copytree(img_path, os.path.join(temp_img_dir, file_name))
 
     path_to_keyIdMap = os.path.join(g.sly_base_dir, "key_id_map.json")
-    shutil.copy(path_to_meta, os.path.join(temp_proj_dir, "meta.json"))
-    shutil.copy(path_to_keyIdMap, os.path.join(temp_proj_dir, "key_id_map.json"))
+    shutil.copy(path_to_meta, os.path.join(proj_dir, "meta.json"))
+    shutil.copy(path_to_keyIdMap, os.path.join(proj_dir, "key_id_map.json"))
 
     remove_dir(g.sly_base_dir)
     os.rename(temp_dir, g.sly_base_dir)
@@ -118,26 +132,9 @@ def sort_episodes_for_kitti():
     meta_json = sly.json.load_json_file(path_to_meta)
     meta = sly.ProjectMeta.from_json(meta_json)
 
-    temp_dir = os.path.join(g.storage_dir, "temp_dir")
-    temp_proj_dir = os.path.join(temp_dir, g.project_name)
-    temp_train_dir = os.path.join(temp_proj_dir, "training")
-    temp_test_dir = os.path.join(temp_proj_dir, "testing")
-    temp_train_ann_path = os.path.join(temp_train_dir, "annotation.json")
-    temp_test_ann_path = os.path.join(temp_test_dir, "annotation.json")
-    temp_train_pcd_dir = os.path.join(temp_train_dir, "pointcloud")
-    temp_test_pcd_dir = os.path.join(temp_test_dir, "pointcloud")
-    temp_train_img_dir = os.path.join(temp_train_dir, "related_images")
-    temp_test_img_dir = os.path.join(temp_test_dir, "related_images")
-
-    sly.fs.mkdir(temp_dir, remove_content_if_exists=True)
-    sly.fs.mkdir(temp_proj_dir)
-    sly.fs.mkdir(temp_train_dir)
-    sly.fs.mkdir(temp_test_dir)
-
-    sly.fs.mkdir(temp_train_pcd_dir)
-    sly.fs.mkdir(temp_test_pcd_dir)
-    sly.fs.mkdir(temp_train_img_dir)
-    sly.fs.mkdir(temp_test_img_dir)
+    temp_dir, proj_dir, train_paths, test_paths = create_temp_dirs(episodes=True)
+    train_dir, train_pcd_dir, train_img_dir, _, train_ann = train_paths
+    test_dir, test_pcd_dir, test_img_dir, _, test_ann = test_paths
 
     datasets = [
         ds for ds in os.listdir(g.sly_base_dir) if os.path.isdir(os.path.join(g.sly_base_dir, ds))
@@ -153,62 +150,43 @@ def sort_episodes_for_kitti():
         train_frames = [frame.index for frame in ann.frames]
         test_frames = [frame for frame in all_frames if frame not in train_frames]
 
-        img_dir = os.path.join(g.sly_base_dir, ds, "related_images")
+        related_img_dir = os.path.join(g.sly_base_dir, ds, "related_images")
 
-        train_frame_pcd_map = {}
-        frames = []
-        for idx, frame in enumerate(train_frames):
-            figures = ann.get_figures_on_frame(frame)
-            new_frame = sly.PointcloudEpisodeFrame(idx, figures)
-            frames.append(new_frame)
+        def sort_episode_items(frames, ds_dir, ann, pcd_dir, img_dir, ann_path):
+            frame2pcd = {}
+            sly_frames = []
+            for idx, frame in enumerate(frames):
+                figures = ann.get_figures_on_frame(frame)
+                new_frame = sly.PointcloudEpisodeFrame(idx, figures)
+                sly_frames.append(new_frame)
 
-            train_frame_pcd_map[idx] = frame_pcd_map[str(frame)]
+                frame2pcd[idx] = frame_pcd_map[str(frame)]
 
-            pcd_name = frame_pcd_map[str(frame)]
-            pcd_path = os.path.join(g.sly_base_dir, ds, "pointcloud", pcd_name)
-            if not os.path.isfile(pcd_path):
-                continue
-            shutil.copy(pcd_path, os.path.join(temp_train_pcd_dir, pcd_name))
+                pcd_name = frame_pcd_map[str(frame)]
+                pcd_path = os.path.join(g.sly_base_dir, ds, "pointcloud", pcd_name)
+                if not os.path.isfile(pcd_path):
+                    continue
+                shutil.copy(pcd_path, os.path.join(pcd_dir, pcd_name))
 
-            if os.path.isdir(img_dir):
-                related_img_dir_name = sly.fs.get_file_name(pcd_name) + "_pcd"
-                img_dirpath = os.path.join(img_dir, related_img_dir_name)
-                if os.path.isdir(img_dirpath):
-                    dest_imgdir = os.path.join(temp_train_img_dir, related_img_dir_name)
-                    shutil.copytree(img_dirpath, dest_imgdir)
+                if os.path.isdir(related_img_dir):
+                    img_dir_name = sly.fs.get_file_name(pcd_name) + "_pcd"
+                    img_dirpath = os.path.join(related_img_dir, img_dir_name)
+                    if os.path.isdir(img_dirpath):
+                        dest_imgdir = os.path.join(img_dir, img_dir_name)
+                        shutil.copytree(img_dirpath, dest_imgdir)
 
-        test_frame_pcd_map = {}
-        for idx, frame in enumerate(test_frames):
-            test_frame_pcd_map[idx] = frame_pcd_map[str(frame)]
-            pcd_name = frame_pcd_map[str(frame)]
-            pcd_path = os.path.join(g.sly_base_dir, ds, "pointcloud", pcd_name)
-            if not os.path.isfile(pcd_path):
-                continue
-            shutil.copy(pcd_path, os.path.join(temp_test_pcd_dir, pcd_name))
+            frames = sly.PointcloudEpisodeFrameCollection(sly_frames)
+            new_ann = ann.clone(len(frames), frames=frames)
 
-            if os.path.isdir(img_dir):
-                related_img_dir_name = sly.fs.get_file_name(pcd_name) + "_pcd"
-                img_dirpath = os.path.join(img_dir, related_img_dir_name)
-                if os.path.isdir(img_dirpath):
-                    dest_imgdir = os.path.join(temp_test_img_dir, related_img_dir_name)
-                    shutil.copytree(img_dirpath, dest_imgdir)
+            sly.json.dump_json_file(new_ann.to_json(), ann_path)
+            sly.json.dump_json_file(frame2pcd, os.path.join(ds_dir, "frame_pointcloud_map.json"))
 
-        frames = sly.PointcloudEpisodeFrameCollection(frames)
-        train_ann = ann.clone(len(frames), frames=frames)
-        test_ann = ann.clone(len(test_frames), frames=sly.PointcloudEpisodeFrameCollection([]))
+        sort_episode_items(train_frames, train_dir, ann, train_pcd_dir, train_img_dir, train_ann)
+        sort_episode_items(test_frames, test_dir, ann, test_pcd_dir, test_img_dir, test_ann)
 
-        sly.json.dump_json_file(train_ann.to_json(), temp_train_ann_path)
-        sly.json.dump_json_file(test_ann.to_json(), temp_test_ann_path)
-
-        sly.json.dump_json_file(
-            train_frame_pcd_map, os.path.join(temp_train_dir, "frame_pointcloud_map.json")
-        )
-        sly.json.dump_json_file(
-            test_frame_pcd_map, os.path.join(temp_test_dir, "frame_pointcloud_map.json")
-        )
     path_to_keyIdMap = os.path.join(g.sly_base_dir, "key_id_map.json")
-    shutil.copy(path_to_meta, os.path.join(temp_proj_dir, "meta.json"))
-    shutil.copy(path_to_keyIdMap, os.path.join(temp_proj_dir, "key_id_map.json"))
+    shutil.copy(path_to_meta, os.path.join(proj_dir, "meta.json"))
+    shutil.copy(path_to_keyIdMap, os.path.join(proj_dir, "key_id_map.json"))
 
     remove_dir(g.sly_base_dir)
     os.rename(temp_dir, g.sly_base_dir)
